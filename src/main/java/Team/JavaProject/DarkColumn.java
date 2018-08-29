ca642c881608f3516375b1a7728a098e2ebf69f2
Krzysiek Gonet <gonetkrzysiek@gmail.com>
2018-08-29 19:51:34 +0200

    Added interfaces and theirs implementations e.g Column classes

--- src/main/java/Team/JavaProject/DarkColumn.java
+++
@@ -1,4 +0,0 @@
-package Team.JavaProject;
-
-public class DarkColumn {
-}
--- src/test/java/Team/JavaProject/BlackColumnUnitTest.java
+++ src/test/java/Team/JavaProject/BlackColumnUnitTest.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 public class DarkColumnUnitTest
 {
         @Test
-        public void randHeight__set_random_column_height__height_should_be_less_than_200()
+        public void RandHeight__set_random_column_height__height_should_be_less_than_200()
         {
             DarkColumn column = new DarkColumn();
             int excpectedMaxColumnHeight = 199;
 
-            column.randHeight();
+            column.RandHeight();
             int columnHeight = column.getH();
             boolean columnHeightLessThan200 = columnHeight <= excpectedMaxColumnHeight;
 
